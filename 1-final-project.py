83dae910ca915d9b9d595d20f8a7f40683aae7bd
Mihir Chakma <91591055+mihirchakma@users.noreply.github.com>
2022-04-30 19:02:29 +0530

    Add files via upload

---
+++ 1-final-project.py
@@ -0,0 +1,8 @@
+# Working with Variables - Final Project 
+
+print("Welcome to Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
+
+
